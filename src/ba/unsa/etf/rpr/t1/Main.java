74b460402dc6d5b1d8111ed3a9cba3c53bfcbf62
ahaskic1 <ahaskic1@etf.unsa.ba>
2019-10-16 16:20:59 +0200

    Kraj zadatka

---
+++ src/ba/unsa/etf/rpr/t1/Main.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr.t1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Sat s = new Sat(15,30,45);
+        s.ispisi();
+        s.sljedeci();
+        s.ispisi();
+        s.pomjeriZa(-48);
+        s.ispisi();
+        s.postavi(0,0,0);
+        s.ispisi();
+
+    }
+}
